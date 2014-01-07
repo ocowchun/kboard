3c3c66f8560915c838bd14b89c1ca735a52c13f9
ocowchun <ocowchun@gmail.com>
2014-01-07 20:55:45 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kboard::Application.config.secret_key_base = '2d39a03bfbd7f5b744e301d9319254cf9770aff47cf9b9cdd17eef76493f1f0375b5079c8c41c60f3a13de7fd977b1ee87d043a211dd75e2d42e123072021d7f'
